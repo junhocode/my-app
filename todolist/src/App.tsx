5cda0fbe28bb5a562d10849f2bf7ee6d2962e49b
rabbithole333 <theworstvalentine@gmail.com>
2024-10-30 17:10:58 +0900

    tsx commit

---
+++ todolist/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+//   let name = "리액트"
+
+//   return (
+//    <div className="container">
+//       <h1 className = "test">Hello,
+//         {
+//           name === '리액트' ? (<h1>Yes</h1>) : (<h1>No</h1>)
+//         }!</h1>
+//     <p>반갑습니다.</p>
+//    </div>
+//   );
+// }
+
+const port = undefined;
+
+return (
+  <div>
+  {
+    port || '포트를 설정하지 않았습니다.'
+  }
+  </div>
+)
+};
+
+export default App;
